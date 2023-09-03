a56b985491c251e1a0f3c09b2934a47f65c5d782
Md Golam Moctader <golam.moctader@gmail.com>
2023-09-03 17:58:28 +0300

    comment

--- onlineeducation/settings.py
+++ onlineeducation/settings.py
@@ -130,9 +130,9 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR/'static'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 
 # Default primary key field type
